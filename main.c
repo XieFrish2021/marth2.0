cda00189ec748f06279b04d5afd5c7d258c37764
Xu Zhixuan <155800918+XieFrish2021@users.noreply.github.com>
2024-10-06 19:19:15 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    system("cls");
+	printf("������\n");
+	_sleep (100);
+	printf("����:0\n");
+	_sleep (100);
+	printf("����:25\n");
+	_sleep (100);
+	printf("����:45\n");
+	_sleep (100);
+	printf("����:65\n");
+	_sleep (100);
+	printf("����:75\n");
+	_sleep (100);
+	printf("����:79\n");
+	_sleep (100);
+	printf("����:100\n");
+	_sleep (100);
+	printf("���سɹ�\n");
+	{
+		while(1)
+		{
+		int a = 20100422;
+		printf("��������Կ");
+		scanf("%d",&a);
+		if(a == 20100422)
+		{
+			printf("��Կ�ɹ�\n");
+			system("cls");
+			_sleep (1000);
+			printf("���ز˵�\n");
+			{
+				while(1)
+				{
+					int x;
+					printf("************C���Բ˵�*********\n");
+					printf("************1�ǿ�ʼ\n");
+					printf("************2��help\n");
+					printf("************3��exit\n");
+					printf("******************************\n");
+					printf("��������ѡ��:");
+					scanf("%d",&x);
+					switch (x)
+					{
+					case 1:
+						printf("���⿪ʼ\n");
+						printf("1,2,3\n");
+						_sleep (1000);
+						while(1)
+						{
+							printf("��ʼ\n");
+							while(1)
+							{
+								int g = 1239;
+								printf("1,1000��239���ڼ�");
+								scanf("%d",&g);
+								if(g == 1239)
+								{
+									printf("����\n");
+									_sleep (100);
+									while(1)
+									{
+										int j = 1235;
+										printf("2,1000��1235˭��");
+										scanf("%d",&j);
+										if(j == 1235)
+										{
+											printf("����\n");
+											{
+												{
+													short d;
+													printf("����������������մ���:");
+													scanf("%s",&d);
+												}
+											while (1)
+											{
+												int r;
+												printf("���������ʲô����(1���˳�)");
+												scanf("%d",&r);
+												switch (r)
+												{
+												case 1:
+													system("cls");
+													exit(0);
+												default :
+													printf("û����Ҫ��ѡ��\n");
+													continue ;
+													}
+													}
+												}
+											}
+										else
+										{
+											printf("��������\n");
+											continue ;
+											}
+										}
+									}
+								else
+								{
+									printf("���ˣ�����\n");
+									continue ;
+									}
+								}
+							}
+					case 2:
+						printf("����:��������\n");
+						printf("v2.0\n");
+						printf("2021,9,21\n");
+						printf("����:www.yuanio.tk\n");
+						continue ;
+					case 3:
+						system("cls");
+						_sleep (1000);
+						exit(0);
+						break ;
+					default :
+						printf("���������ѡ\n");
+						continue ;
+						}
+					}
+				}
+			}
+		else
+		{
+			printf("��Կʧ��\n");
+			continue ;
+			}
+		}
+	}
+	return 0;
+	system("pause");
+}
